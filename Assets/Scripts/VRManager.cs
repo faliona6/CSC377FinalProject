9bb33a99c0327e56c49266324c87834655f857bb
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2021-03-03 00:54:26 -0800

    init

---
+++ Assets/Scripts/VRManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRManager : MonoBehaviour
+{
+    public bool showVR = false;
+
+    private void Update()
+    {
+        UnityEngine.XR.XRSettings.showDeviceView = true;
+    }
+
+}
